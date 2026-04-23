db21a6d4e40192e774a0562431b6e8bfe2dfb862
Vandanarajput <145121571+Vandanarajput@users.noreply.github.com>
2026-04-23 09:32:25 +0530

    init

--- ReactLearn/src/App.jsx
+++ ReactLearn/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Bookmark} from 'lucide-react'
+import { Bookmark } from 'lucide-react'
 
 const App = () => {
   return (
@@ -8,11 +8,11 @@ const App = () => {
 
         <div className="top">
           <img src="https://thumbs.dreamstime.com/b/amazon-logo-editorial-illustrative-white-background-eps-download-vector-jpeg-banner-ai-amazon-logo-editorial-illustrative-208329107.jpg" alt="Amazon Logo" />
-          <button>Save <Bookmark  size={12}/></button>
+          <button>Save <Bookmark size={12} /></button>
         </div>
 
         <div className="center">
-          <h1>Amazon <span>5 days</span></h1>
+          <h1>Amazon <span>5 days ago</span></h1>
           <h2>Senior UI/UX Designer</h2>
           <div>
             <h4>Part time</h4>
@@ -20,15 +20,16 @@ const App = () => {
           </div>
         </div>
 
-
-
         <div className="bottom">
-          <h3>$120/hr</h3>
-          <p>Mumbai,India</p>
+          <div>
+            <h3>$120/hr</h3>
+            <p>Mumbai,India</p>
+          </div>
+          <button>Apply now</button>
         </div>
-        <button>Apply now</button>
+
       </div>
-      
+
     </div>
   )
 }
